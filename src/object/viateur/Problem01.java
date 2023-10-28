fcfcb73f20315c943f4d0727c4b46053eb445fcd
Viateur <akimanaviateur94@gmail.com>
2023-10-28 22:01:38 +0200

    calling superclass constructor

--- src/object/viateur/Problem01.java
+++ src/object/viateur/Problem01.java
@@ -1,10 +1,9 @@
 package object.viateur;
 // Class A with a string property called message and a method called hello
-// Class A with a string property called message and a method called hello
 class A {
     protected String message; // protected so that it can be accessed by subclass B
 
-    // Constructor to initialize the message property
+    // Constructor to initialize the message property with a parameter
     public A(String message) {
         this.message = message;
     }
@@ -17,26 +16,24 @@ class A {
 
 // Class B which extends class A
 class B extends A {
-    // Constructor to initialize the message property through the superclass constructor
-    public B(String message) {
-        super(message);
+    // Default constructor for class B
+    public B() {
+        super("Hello from B"); // Call superclass constructor with a default message
     }
 
-    // Override the message variable in class B
-    @Override
-    public void hello() {
-        super.hello(); // Call the hello method of class A
-        System.out.println("Hello from class B: " + message);
-    }
+    // Constructor with a parameter for class B (if needed)
+    // public B(String customMessage) {
+    //     super(customMessage);
+    // }
 }
 
 // Class Exercise01 with a main method
 public class Problem01 {
     public static void main(String[] args) {
-        // Create an instance of class B and call the hello method
-        B objectB = new B("Message from class B!");
-        objectB.hello();
-        // Output: Message from class B!
-        //         Hello from class B: Message from class B!
+        // Create an instance of class B
+        B objectB = new B();
+
+        // Call the hello method on the object of class B
+        objectB.hello(); // Output: Hello from B
     }
 }
